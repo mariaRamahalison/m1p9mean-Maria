38971a0ef16c16ce9e5e0b5e506b7df1cf377ce9
Maria Ramahalison <mariaramahalison@gmail.com>
2022-04-11 12:00:19 +0300

    data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -18,7 +18,7 @@ export class DataService {
     headers: new HttpHeaders({
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' 
+      'Authorization': 'Bearer ' + (this.storageService.getLocalStorage("USER_DETAIL"))?.token || null
     })
   };
 
